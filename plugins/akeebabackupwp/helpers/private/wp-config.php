c3bc5920b6cd03b31b2d5e696863be39d728b438
gohilrakesh <118576394+gohilrakesh@users.noreply.github.com>
2022-11-19 14:11:52 -0500

    Reset

--- plugins/akeebabackupwp/helpers/private/wp-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-global $table_prefix;
-$table_prefix = 'wp_';
-define('DB_NAME', 'airapps_works');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
--- plugins/html-forms/code-snippets/add-custom-attribute-to-form-tag.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-add_filter(
-	'hf_form_html',
-	function( $html ) {
-		$html = str_replace( '<form ', '<form ms-signup="true" ', $html );
-		return $html;
-	}
-);
